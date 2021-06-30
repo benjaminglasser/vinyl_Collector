eadcd3ff6fd90d60487ed30531ea53d457a3a555
ben glasser <ben@fantasticvoyage.com>
2021-06-30 17:40:04 -0400

    adds view all records

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+
+class Vinyl:
+    def __init__(self, artist, title, year_of_release, genre):
+        self.artist = artist
+        self.title = title
+        self.year_of_release = year_of_release
+        self.genre = genre
+
+
+vinyl = [
+    Vinyl('The Rolling Stones', 'Some Girls', 1978, 'Rock'),
+    Vinyl('Nas', 'Illmatic', 1994, 'Hip-Hop'),
+    Vinyl('The Flaming Lips', 'The Soft Bulletin', 1999, 'Alternative/Indie'),
+    Vinyl('Floating Points', 'Crush', 2019, 'Electronic'),
+]
+
+
 # Create your views here.
 
 
@@ -9,3 +26,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def vinyl_index(request):
+    return render(request, 'vinyl/index.html', {'vinyl': vinyl})
