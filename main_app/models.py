db66dfa4d42253104c9704eb11e2ba72313cae89
ben glasser <ben@fantasticvoyage.com>
2021-07-15 17:22:54 -0700

    adds Artist Model

--- main_app/models.py
+++ main_app/models.py
@@ -3,11 +3,19 @@ from django.db import models
 # Create your models here.
 
 
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    members = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+
 class Vinyl(models.Model):
-    artist = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=100)
     year = models.IntegerField()
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.artist
+        return self.title
